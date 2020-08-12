b675f9ad67b22b63c861b929b7c1c48a90d7c9e6
joaomcode <joaomarcelo.co@hotmail.com>
2020-08-11 22:02:03 -0300

    Aula 03 - Finalizando o front-end

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/56600657?s=460&u=46c8359adb0ba0540efcf0443e83fb60fdeef673&v=4" alt="joaomcode" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>joaomcode</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de Física avançada
-      <br /> <br />
-      Apaixonado por física clássica newtoniana e Física moderna. Experimentar é conhecer e aprender, venha conhecer a física de modo didático e descontraído por meio de experiências.
-    </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-        <strong>R$80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          type="button"
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
         Entrar em contato
-      </button>
+        </a>
       </footer>
     </article>
   )
